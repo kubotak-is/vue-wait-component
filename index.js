c4a0fa3059886a7af632ad161015c0e18e240605
kubotak-is <kubotak@istyle.co.jp>
2018-06-24 23:11:58 +0900

    modify: export

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import WaitList from './src/components/WaitListComponent.vue'
 import WaitImg from './src/components/WaitImgComponent.vue'
 
-export default { WaitList, WaitImg };
+export { WaitList, WaitImg };
